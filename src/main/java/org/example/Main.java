d0fe608e562f26f0a1c884e1260510d0df38c3a1
jaxpro-code <jax.bradley.pro@gmail.com>
2026-04-09 13:29:15 -0400

    commit test 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,7 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
+        System.out.println("hey i broke my computer trying to get ahead and i fixed it i think and im scared now");
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
