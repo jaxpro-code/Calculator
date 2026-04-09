10b5053993e4995866defd22df8e402619643bf4
jaxpro-code <jax.bradley.pro@gmail.com>
2026-04-09 13:44:49 -0400

    commit and push test 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,6 +9,7 @@ public class Main {
         System.out.printf("Hello and welcome!");
         System.out.println("hey i broke my computer trying to get ahead and i fixed it i think and im scared now");
         System.out.println("this is stressful. ahhhhahahah");
+        System.out.println("my last push failed so lets try again");
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
